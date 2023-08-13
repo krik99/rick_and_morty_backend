2cf352c6aff2a706867e030f2dd326d63f12cfb3
Krik99 <krik992@gmail.com>
2023-08-13 14:59:28 +0300

    <Common>: Add cors support for API

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const cors = require('cors');
 require('./database/mongo');
 
 const charactersRoutes = require('./routes/characters');
@@ -7,6 +7,7 @@ const charactersRoutes = require('./routes/characters');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use('/v1/characters', charactersRoutes);
 
 app.listen(port, () => {
